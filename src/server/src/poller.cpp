cebdb4c394d4ad8e91774a4b6f0535e60ccde728
Jordan <121682392+jordan-sissilian@users.noreply.github.com>
2024-09-03 09:47:38 +0200

    chore(server): remove windows compatibility on poller

--- src/server/src/poller.cpp
+++ src/server/src/poller.cpp
@@ -16,12 +16,6 @@ Poller::Poller() {
         std::cerr << "Erreur lors de la création de kqueue: " << std::strerror(errno) << std::endl;
         exit(EXIT_FAILURE);
     }
-#elif _WIN32
-    iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
-    if (iocp == NULL) {
-        std::cerr << "Erreur lors de la création de IOCP: " << std::strerror(errno) << std::endl;
-        exit(EXIT_FAILURE);
-    }
 #endif
 }
 
@@ -30,8 +24,6 @@ Poller::~Poller() {
     close(epoll_fd);
 #elif __APPLE__
     close(kqueue_fd);
-#elif _WIN32
-    CloseHandle(iocp);
 #endif
 }
 
@@ -45,9 +37,6 @@ bool Poller::add(int fd) {
     struct kevent ev;
     EV_SET(&ev, fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
     return kevent(kqueue_fd, &ev, 1, NULL, 0, NULL) == 0;
-#elif _WIN32
-    HANDLE handle = (HANDLE)fd;
-    return CreateIoCompletionPort(handle, iocp, (ULONG_PTR)fd, 0) != NULL;
 #else
     return false;
 #endif
@@ -60,9 +49,6 @@ bool Poller::remove(int fd) {
     struct kevent ev;
     EV_SET(&ev, fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
     return kevent(kqueue_fd, &ev, 1, NULL, 0, NULL) == 0;
-#elif _WIN32
-    // Pour IOCP, la suppression se fait généralement en fermant le socket
-    return true;
 #else
     return false;
 #endif
@@ -80,13 +66,6 @@ void Poller::wait(std::function<void(int)> onEvent) {
     int nev = kevent(kqueue_fd, NULL, 0, events, 10, NULL);
     for (int i = 0; i < nev; ++i) {
         onEvent(events[i].ident);
-    }
-#elif _WIN32
-    DWORD bytesTransferred;
-    ULONG_PTR completionKey;
-    LPOVERLAPPED overlapped;
-    while (GetQueuedCompletionStatus(iocp, &bytesTransferred, &completionKey, &overlapped, INFINITE)) {
-        onEvent(static_cast<int>(completionKey));
     }
 #endif
 }
